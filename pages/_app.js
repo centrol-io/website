6ee99a399367e1eb342e8c97f996b5d14373a0d4
Mike Shimonov <mike@centrol.io>
2024-10-14 17:57:22 -0400

    favicon + logo added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'  // Adjust the path if needed
+import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/logo-192x192.png" />
+        <link rel="apple-touch-icon" href="/logo-192x192.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
